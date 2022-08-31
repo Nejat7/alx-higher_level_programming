a7314d100c8a35ef542d4c2c5874694ba6d0673d
Nejat Dedgeba <nejatdoc@gmail.com>
2022-08-31 11:30:20 -0700

    hot fix

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-
 def number_keys(a_dictionary):
-
-    l = len(list(a_dictionary.keys()))
-
-    return l
+    i = 0
+    for x in a_dictionary:
+        i += 1
+    return (i)
